5e0b707b5df32b9437eba8d670f9e7b495d6e244
Marcos Minond <minond.marcos@gmail.com>
2019-03-13 17:27:17 -0600

    Client middleware base

---
+++ lib/protobuf/rpc/client/middleware.rb
@@ -0,0 +1,23 @@
+require 'middleware'
+require 'middleware/runner'
+
+require 'protobuf/rpc/client/middleware/logger'
+require 'protobuf/rpc/client/middleware/request_encoder'
+require 'protobuf/rpc/client/middleware/response_decoder'
+
+module Protobuf
+  module Rpc
+    def self.client_middleware
+      @client_middleware ||= ::Middleware::Builder.new(:runner_class => ::Middleware::Runner)
+    end
+
+    # Ensure the middleware stack is initialized
+    client_middleware
+  end
+
+  Rpc.client_middleware.use(::Protobuf::Rpc::Middleware::Client::Logger)
+  Rpc.client_middleware.use(::Protobuf::Rpc::Middleware::Client::RequestEncoder)
+  Rpc.client_middleware.use(::Protobuf::Rpc::Middleware::Client::ResponseDecoder)
+
+  ActiveSupport.run_load_hooks(:protobuf_rpc_client_middleware, Rpc)
+end
